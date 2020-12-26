246043faa58299b9dad3a9ded4518b077d9e16a7
brian91292 <8050523+brian91292@users.noreply.github.com>
2020-12-26 13:07:46 -0600

    begin switching over to implicit auth

--- release/githubio/login/twitch.js
+++ release/githubio/login/twitch.js
@@ -1,7 +1,8 @@
 /* jshint eqeqeq: true, undef: true, unused: true, node: true, browser: true */
 
 var baseUrlCode = 'https://id.twitch.tv/oauth2/authorize?';
-var type_code = 'code';
+//var type_code = 'code';
+var type_token = 'token id_token';
 var client_id = 'p3s4bbfcwe6bba581z0coq40wnycs6';
 var redirect_uri = 'https://brian91292.github.io/smarttv-twitch/release/githubio/login/twitch.html';
 var scope = 'user_read+user_follows_edit+user_subscriptions';
@@ -23,18 +24,18 @@ function ProcessStart() {
 
 function autorizationClick() {
     console.log("autorizationClick");
-    var url = baseUrlCode + 'response_type=' + type_code + '&client_id=' + encodeURIComponent(client_id) +
+    var url = baseUrlCode + 'response_type=' + type_token + '&client_id=' + encodeURIComponent(client_id) +
         '&redirect_uri=' + redirect_uri + '&scope=' + scope + '&force_verify=' + force_verify;
     window.location = url;
 }
 
 function processCode(pageUrl) {
     console.log("processCode");
-    code = pageUrl.match(/code=(\w+)/);
+    code = pageUrl.match(/access_token=(\w+)/);
     if (code) {
         code = code[1];
         document.getElementById('Authorize').classList.add('hidden');
-        document.getElementById("PageText").innerHTML = 'Enter the bellow key in the above screen of the app (There is only lower case letters and numbers)<br><div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">Key:<br><br>' + code + '<br></div>';
+        document.getElementById("PageText").innerHTML = 'Enter the below key in the above screen of the app (There is only lower case letters and numbers)<br><div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">Key:<br><br>' + code + '<br></div>';
         console.log('if code ' + code);
     } else {
         document.getElementById("PageText").innerHTML = '<div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">An error occurred, please try again, if the error persists contact the developer in the bellow github link</div>';
